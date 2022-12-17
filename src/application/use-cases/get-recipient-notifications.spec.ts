c12646a4c1dfd8554e3599c7c503cb446006331f
Andrei Bissolotti <andreibisso@gmail.com>
2022-12-17 19:57:19 -0300

    Lista as notificações de um recipient

--- src/application/use-cases/get-recipient-notifications.spec.ts
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -1,12 +1,12 @@
 import { Notification } from "@application/entities/notification"
 import { makeNotification } from "@test/factories/notification-factory"
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository"
-import { CountRecipientNotifications } from "./count-recipient-notifications"
+import { GetRecipientNotifications } from "./get-recipient-notifications"
 
-describe('count recipient notifications', () => {
-  it('should be able to count recipient notifications', async () => {
+describe('get recipient notifications', () => {
+  it('should be able to get recipient notifications', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository
-    const countRecipientNotifications = new CountRecipientNotifications(notificationsRepository)
+    const getRecipientNotifications = new GetRecipientNotifications(notificationsRepository)
 
     await notificationsRepository.create(
       makeNotification({ recipientId: 'recipient-1' })
@@ -20,8 +20,12 @@ describe('count recipient notifications', () => {
       makeNotification({ recipientId: 'recipient-2' })
     )
 
-    const { count } = await countRecipientNotifications.execute({ recipientId: 'recipient-1' })
+    const { notifications } = await getRecipientNotifications.execute({ recipientId: 'recipient-1' })
 
-    expect(count).toEqual(2)
+    expect(notifications).toHaveLength(2)
+    expect(notifications).toEqual(expect.arrayContaining([
+      expect.objectContaining({ recipientId: 'recipient-1' }),
+      expect.objectContaining({ recipientId: 'recipient-1' })
+    ]))
   })
 })
