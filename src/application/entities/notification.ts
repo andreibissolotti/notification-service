a294b2db486fa8266dea1888f34ddb889899290f
Andrei Bissolotti <andreibisso@gmail.com>
2022-12-17 17:42:19 -0300

    Aula 2

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,16 +1,29 @@
+import { randomUUID } from "crypto";
+import { Replace } from "src/helpers/replace";
+import { Content } from "./conten";
+
 export interface NotificationProps {
   recipientId: string;
-  content: string;
+  content: Content;
   category: string;
   readAt?: Date | null;
   createdAt: Date;
 }
 
 export class Notification {
+  private _id: string
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID()
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    }
+  }
+
+  public get id() {
+    return this._id
   }
 
   public set recipientId(recipientId: string) {
@@ -21,11 +34,11 @@ export class Notification {
     return this.props.recipientId;
   }
 
-  public set content(content: string) {
+  public set content(content: Content) {
     this.props.content = content;
   }
 
-  public get content(): string {
+  public get content(): Content {
     return this.props.content;
   }
 
