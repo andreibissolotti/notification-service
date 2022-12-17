44bc22cdc0461a19c1216b54a5ffa8ab9edf5fb2
Andrei Bissolotti <andreibisso@gmail.com>
2022-12-17 19:22:28 -0300

    Count recipient notifications

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -5,7 +5,7 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
   public notifications: Notification[] = []
 
   async findById(notificationId: string): Promise<Notification | null> {
-    const notification = this.notifications.find(item => item.id === notificationId)
+    const notification = this.notifications.find(notification => notification.id === notificationId)
 
     if (!notification) {
       return null
@@ -14,12 +14,16 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
     return notification
   }
 
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(notification => notification.recipientId === recipientId).length
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification)
   }
 
   async save(notification: Notification): Promise<void> {
-    const notificationIndex = this.notifications.findIndex(item => item.id === notification.id)
+    const notificationIndex = this.notifications.findIndex(notification => notification.id === notification.id)
 
     if (notificationIndex >= 0) {
       this.notifications[notificationIndex] = notification
